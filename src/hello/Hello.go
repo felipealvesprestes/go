2e593fda35ffdf65439d213d5365df57fc7ee08d
Felipe Prestes <felipealvesprestes@gmail.com>
2022-03-31 18:34:13 -0300

    Variables types

--- src/hello/Hello.go
+++ src/hello/Hello.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	var nome string = "Felipe"
+	var idade int = 27
+	var versao float32 = 1.1
+
+	fmt.Println("Olá", nome)
+	fmt.Println("Sua idade é", idade)
+	fmt.Println("A versão do programa é", versao)
 }
