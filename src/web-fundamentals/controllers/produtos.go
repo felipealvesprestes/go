f80b7b483e3b2248bc6c64a79adffaed0816500d
Felipe Prestes <felipealvesprestes@gmail.com>
2022-04-06 18:24:50 -0300

    Feature update product

--- src/web-fundamentals/controllers/produtos.go
+++ src/web-fundamentals/controllers/produtos.go
@@ -48,3 +48,33 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	models.DeleteProductById(productId)
 	http.Redirect(w, r, "/", 301)
 }
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	productId, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	product := models.GetProductById(productId)
+	templatePage.ExecuteTemplate(w, "Edit", product)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		name := r.FormValue("nome")
+		description := r.FormValue("descricao")
+
+		price, err := strconv.ParseFloat(r.FormValue("preco"), 64)
+
+		if err != nil {
+			log.Fatal("Erro na converção do preço.")
+		}
+
+		quantity, err := strconv.Atoi(r.FormValue("quantidade"))
+
+		if err != nil {
+			log.Fatal("Erro na converção da quantidade.")
+		}
+
+		models.UpdateProduct(id, name, description, price, quantity)
+	}
+
+	http.Redirect(w, r, "/", 301)
+}
--- src/web-fundamentals/models/produtos.go
+++ src/web-fundamentals/models/produtos.go
@@ -17,7 +17,7 @@ type Produto struct {
 func FindAllProducts() []Produto {
 	db := config.DbConnect()
 
-	findaAllProducts, err := db.Query("select * from produtos")
+	findaAllProducts, err := db.Query("select * from produtos order by 1 desc")
 
 	if err != nil {
 		log.Fatal(err)
@@ -56,13 +56,13 @@ func FindAllProducts() []Produto {
 func InsertNewProduct(name string, description string, price float64, quantity int) {
 	db := config.DbConnect()
 
-	insertDatabase, err := db.Prepare("insert into produtos (nome, descricao, preco, quantidade) values ($1, $2, $3, $4)")
+	insertProduct, err := db.Prepare("insert into produtos (nome, descricao, preco, quantidade) values ($1, $2, $3, $4)")
 
 	if err != nil {
 		log.Fatal("Erro ao cadastrar o novo produto.")
 	}
 
-	insertDatabase.Exec(name, description, price, quantity)
+	insertProduct.Exec(name, description, price, quantity)
 
 	defer db.Close()
 }
@@ -70,13 +70,63 @@ func InsertNewProduct(name string, description string, price float64, quantity i
 func DeleteProductById(productId string) {
 	db := config.DbConnect()
 
-	deleteDatabase, err := db.Prepare("delete from produtos where id = $1")
+	deleteProduct, err := db.Prepare("delete from produtos where id = $1")
 
 	if err != nil {
 		log.Fatal("Erro ao excluir o produto.")
 	}
 
-	deleteDatabase.Exec(productId)
+	deleteProduct.Exec(productId)
+
+	defer db.Close()
+}
+
+func GetProductById(productId int) Produto {
+	db := config.DbConnect()
+
+	getProduct, err := db.Query("select * from produtos where id = $1", productId)
+
+	if err != nil {
+		log.Fatal("Erro ao buscar as informações do produto.", err)
+	}
+
+	product := Produto{}
+
+	for getProduct.Next() {
+		var id int
+		var name string
+		var description string
+		var price float64
+		var quantity int
+
+		err = getProduct.Scan(&id, &name, &description, &price, &quantity)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		product.Id = id
+		product.Nome = name
+		product.Descricao = description
+		product.Preco = price
+		product.Quantidade = quantity
+	}
+
+	defer db.Close()
+
+	return product
+}
+
+func UpdateProduct(id string, name string, description string, price float64, quantity int) {
+	db := config.DbConnect()
+
+	updateProduct, err := db.Prepare("update produtos set nome = $2, descricao = $3, preco = $4, quantidade = $5 where id = $1")
+
+	if err != nil {
+		log.Fatal("Erro ao cadastrar o novo produto.")
+	}
+
+	updateProduct.Exec(id, name, description, price, quantity)
 
 	defer db.Close()
 }
