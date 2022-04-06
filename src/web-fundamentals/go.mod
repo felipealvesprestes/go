ca986fb453ef840d2687fa5a920b7e19f10f2d87
Felipe Prestes <felipealvesprestes@gmail.com>
2022-04-05 22:37:17 -0300

    Database connection and simple query

--- src/web-fundamentals/go.mod
+++ src/web-fundamentals/go.mod
@@ -1,3 +1,5 @@
 module web-fundamentals
 
 go 1.18
+
+require github.com/lib/pq v1.10.4 // indirect
--- src/web-fundamentals/main.go
+++ src/web-fundamentals/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,11 +25,50 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Básica branca", Descricao: "Camiseta básica branca", Preco: 39.90, Quantidade: 11},
-		{Nome: "Básica preta", Descricao: "Camiseta básica preta", Preco: 37.90, Quantidade: 8},
-		{Nome: "Básica Azul", Descricao: "Camiseta básica azul", Preco: 34.90, Quantidade: 6},
+	db := conectaBanco()
+
+	selectProdutos, err := db.Query("select * from produtos")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	produto := Produto{}
+	produtos := []Produto{}
+
+	for selectProdutos.Next() {
+		var id int
+		var nome string
+		var descricao string
+		var preco float64
+		var quantidade int
+
+		err = selectProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		produto.Nome = nome
+		produto.Descricao = descricao
+		produto.Preco = preco
+		produto.Quantidade = quantidade
+
+		produtos = append(produtos, produto)
 	}
 
 	templatePage.ExecuteTemplate(w, "Index", produtos)
+
+	defer db.Close()
+}
+
+func conectaBanco() *sql.DB {
+	stringConexao := "user=postgres password=postgres dbname=loja_alura host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", stringConexao)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
 }
