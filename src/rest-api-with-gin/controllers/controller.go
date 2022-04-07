260774fe4880b5f46de806c94578d80aa9c21bc1
Felipe Prestes <felipealvesprestes@gmail.com>
2022-04-07 18:44:27 -0300

    Response register not found

--- src/rest-api-with-gin/controllers/controller.go
+++ src/rest-api-with-gin/controllers/controller.go
@@ -37,5 +37,11 @@ func ExibeAluno(c *gin.Context) {
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
 	database.DB.First(&aluno, id)
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Aluno não encontrado",
+		})
+		return
+	}
 	c.JSON(http.StatusOK, aluno)
 }
