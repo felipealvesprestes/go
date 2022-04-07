19cbbd5d55e5ae197a1fe832af83d73a11c44e3b
Felipe Prestes <felipealvesprestes@gmail.com>
2022-04-06 21:47:56 -0300

    Add Gorilla Mux

--- src/rest-api-roots/go.mod
+++ src/rest-api-roots/go.mod
@@ -1,3 +1,5 @@
 module github.com/felipealvesprestes/rest-api-roots
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- src/rest-api-roots/routes/routes.go
+++ src/rest-api-roots/routes/routes.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/felipealvesprestes/rest-api-roots/controllers"
+	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
